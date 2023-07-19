36941ace5e7d4475bf01cf06b765e93f42e281b2
jmakhmudov <makhmudov@tuta.io>
2023-07-19 20:00:30 +0500

    completed

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import './App.css';
+
+
+function App() {
+  const [walletAddress, setWalletAddress] = useState<string>('');
+  const [balance, setBalance] = useState<string>('');
+
+  const handleCopy = (): void => {
+    navigator.clipboard.writeText(walletAddress);
+  };
+
+  const modifyWalletAddress = (address: string): JSX.Element => {
+    let firstSix = address.slice(0, 6);
+    let lastFour = address.slice(-4);
+
+    return <p>Wallet address: <span onClick={handleCopy} className='data address' >{`${firstSix}....${lastFour}`}</span></p>;
+  }
+
+  const requestWallet = async () => {
+    if (window.ethereum) {
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+
+        setWalletAddress(accounts[0])
+      } catch (error) {
+        console.log("Error:", error)
+      }
+
+    } else {
+      console.log("metamask is not found")
+    }
+  }
+
+  const connectWallet = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestWallet();
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+
+      const balance = await provider.getBalance('ethers.eth').then((num) => {
+        return ethers.formatEther(num);
+      });
+
+      setBalance(balance);
+    }
+  }
+
+  return (
+    <main>
+      <h1>Ethers.js and React Integration</h1>
+      <button onClick={connectWallet}>Connect Wallet</button>
+      {
+        walletAddress ?
+          modifyWalletAddress(walletAddress)
+          :
+          <div></div>
+      }
+    </main>
+  )
+}
+
+export default App
